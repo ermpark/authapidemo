08eb8a80233dc66068abe5ca9db4bfcd3ad2ec18
wpp <wpp@qq.com>
2018-09-15 16:37:48 +0800

    init commit

---
+++ AuthAPI/Controllers/TokenController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AuthAPI.Models;
+using IdentityModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        // private MainDBContext _context;
+        [HttpPost]
+        public async Task<IActionResult> GenToken(UserModel model)
+        {
+            //get username and pwd from this model ,then get data from db 
+            //var user = _context.Users.FirstOrDefault(l => l.Name == model.Name && l.Pwd == model.PassWord);
+            //If there is data in the database
+            var user = new UserModel() { ID = 1, Email = "example@live.com", Name = "tester", PassWord = "123456" };
+            //if user null ,return
+            if (user == null) return Unauthorized();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("this is a security key");
+            var authTime = DateTime.UtcNow;
+            var expiresAt = authTime.AddDays(7);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(JwtClaimTypes.Audience,"api"),
+                    new Claim(JwtClaimTypes.Issuer,"http://localhost:44319"),
+                    new Claim(JwtClaimTypes.Id, user.ID.ToString()),
+                    new Claim(JwtClaimTypes.Name, user.Name)
+                }),
+                Expires = expiresAt,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(new
+            {
+                access_token = tokenString,
+                token_type = "Bearer",
+                profile = new
+                {
+                    sid = user.ID,
+                    name = user.Name,
+                    auth_time = new DateTimeOffset(authTime).ToUnixTimeSeconds(),
+                    expires_at = new DateTimeOffset(expiresAt).ToUnixTimeSeconds()
+                }
+            });
+        }
+    }
+}
---
+++ AuthAPI/Startup.cs
@@ -0,0 +1,78 @@
+﻿using IdentityModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AuthAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            #region Auth 
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+               .AddJwtBearer(o =>
+               {
+                   o.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       NameClaimType = JwtClaimTypes.Name,
+                       RoleClaimType = JwtClaimTypes.Role,
+
+                       ValidIssuer = "http://localhost:44319",
+                       ValidAudience = "api",
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("this is a security key"))
+
+                       /***********************************TokenValidationParameters的参数默认值***********************************/
+                       // RequireSignedTokens = true,
+                       // SaveSigninToken = false,
+                       // ValidateActor = false,
+                       // 将下面两个参数设置为false，可以不验证Issuer和Audience，但是不建议这样做。
+                       // ValidateAudience = true,
+                       // ValidateIssuer = true, 
+                       // ValidateIssuerSigningKey = false,
+                       // 是否要求Token的Claims中必须包含Expires
+                       // RequireExpirationTime = true,
+                       // 允许的服务器时间偏移量
+                       // ClockSkew = TimeSpan.FromSeconds(300),
+                       // 是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
+                       // ValidateLifetime = true
+                   };
+               });
+            #endregion
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
